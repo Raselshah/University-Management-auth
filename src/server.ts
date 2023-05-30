c6e149a3c7cee79cf9c27b6dce775ea2d03a3b88
RaselShah <mdraselshah56@gmail.com>
2023-05-31 01:29:14 +0600

    check husky setup

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 // getting-started.js
-import mongoose from "mongoose";
-import app from "./api";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function run() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(` Database connected successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(` Database connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (err) {
     // console.log(`faild database connect ${err.message}`);
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-run().catch((err) => console.log(err));
+run().catch(err => console.log(err))
